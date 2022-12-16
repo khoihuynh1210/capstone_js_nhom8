77d943079d2a1f3e7d61c2c1ca6d4a2e6c54ce37
khoihuynh1210 <khoihuynh1210@gmail.com>
2022-12-16 22:51:23 +0700

    done

--- src/services/productService.js
+++ src/services/productService.js
@@ -8,4 +8,11 @@ function ProductService() {
 
         return promise;
     };
+
+    this.deleteProductApi = function (id) {
+        return axios({
+            url: `https://637b69996f4024eac20ce110.mockapi.io/api/product?fbclid=IwAR2JWKVKwzsd7hnDAL7pYyz9lzMiB_jJ7IEWYkssuNaoCSaVbTOIYXZ9_1A/${id}`,
+            method: "DELETE",
+        });
+    };
 }
